e7c341462914b7325c194f226eff7729d0061f18
Sunil-MI <sunillulla546@yahoo.com>
2018-03-23 14:28:07 +0530

    changes

--- script3.jsx
+++ script3.jsx
@@ -1,11 +1,12 @@
 var styles = {color:'red'}
+var styles2 = {color:'yellow'}
 var MyComponent = React.createClass({
 	render:function(){
 		return (
 			<div style={styles}>
 				<h1> Multiple </h1>
 				<h1> Components </h1>
-				<h1> Demo </h1>
+				<h1 style={styles2}> Demo </h1>
 				<h1> with </h1>
 				<h1> style </h1>
 				<h1> attribute </h1>
