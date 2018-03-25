74316beaf088406a470a46f29178a1d6b7b8969d
Sunil-MI <sunillulla546@yahoo.com>
2018-03-25 23:17:03 +0530

    stateless example added

---
+++ Reactdemo/App2.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class App extends React.Component {
+	render(){
+		return(
+				<div>
+					<Header />
+					<Footer />
+				</div>
+			)
+	}
+}
+
+
+class Header extends React.Component {
+	render(){
+		return(
+				<div>
+					<h1> Header </h1>
+				</div>
+			)
+	}
+}
+
+class Footer extends React.Component {
+	render(){
+		return(
+				<div>
+					<h1> Footer </h1>
+				</div>
+			)
+	}
+}
+
+export default App;
